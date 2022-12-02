8872b1398f73488b1c09e1922edd1d102d708f59
Ishani Parekh <ishani@google.com>
2022-12-01 23:31:58 -0800

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return input.sortedDescending().subList(0, 3).sum()
+    }
+
+    fun computeCaloriesForElves(input: List<String>): List<Int> {
+        var curVal = 0
+        val output = mutableListOf<Int>()
+        for (row in input) {
+            val rowAsInt = row.toIntOrNull()
+            if (rowAsInt != null) {
+                curVal += rowAsInt
+            } else {
+                output.add(curVal)
+                curVal = 0
+            }
+        }
+        output.add(curVal)
+        return output
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testCalories = computeCaloriesForElves(testInput)
+    check(part1(testCalories) == 6000)
+    check(part2(testCalories) == 10000)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01_input")
+    val inputCalories = computeCaloriesForElves(input)
+    println(part1(inputCalories))
+    println(part2(inputCalories))
 }
