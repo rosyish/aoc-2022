82c2e1d486ecbc015331ca054492401d73457b4b
Ishani Parekh <ishani@google.com>
2022-12-03 23:28:54 -0800

    improvements

--- src/Day04.kt
+++ src/Day04.kt
@@ -6,6 +6,14 @@ fun overlaps(x: IntRange, y: IntRange): Boolean {
     return x.intersect(y).isNotEmpty()
 }
 
+val efficientFullyContains = fun(x: IntRange, y: IntRange): Boolean {
+    return (x.first <= y.first && x.last >= y.last) || (y.first <= x.first && y.last >= x.last)
+}
+
+val efficientOverlaps = fun(x: IntRange, y: IntRange): Boolean {
+    return x.contains(y.first) || y.contains(x.first)
+}
+
 fun main() {
     fun mapToRanges(str : String) : List<IntRange> {
         return Regex("(\\d+)-(\\d+),(\\d+)-(\\d+)")
@@ -17,8 +25,8 @@ fun main() {
     }
 
     val input = readInput("Day04_input").map(::mapToRanges)
-    val part1Result =  input.count { fullyContains(it[0], it[1]) }
-    val part2Result = input.count { overlaps(it[0], it[1]) }
+    val part1Result =  input.count { efficientFullyContains(it[0], it[1]) }
+    val part2Result = input.count { efficientOverlaps(it[0], it[1]) }
     println(part1Result)
     println(part2Result)
 }
