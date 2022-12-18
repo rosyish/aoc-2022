e855a68e94218bb9676a69008f38d481f63817b5
Ishani Parekh <ishani@google.com>
2022-12-17 23:05:50 -0800

    use typealias

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,22 +1,21 @@
-import kotlin.math.abs
-
+typealias Cube = Triple<Int, Int, Int>
 fun main() {
     val cubes = readInput("Day18_input").map {
         val (x, y, z) = it.split(",").map { it.toInt() }
-        Triple(x, y, z)
+        Cube(x, y, z)
     }.toSet()
     val minDim = cubes.flatMap { it.toList() }.min()
     val maxDim = cubes.flatMap { it.toList() }.max()
 
-    fun Triple<Int, Int, Int>.getAdjacent(): List<Triple<Int, Int, Int>> {
+    fun Cube.getAdjacent(): List<Cube> {
         return listOf(
-            Triple(0, 0, 1),
-            Triple(0, 0, -1),
-            Triple(0, 1, 0),
-            Triple(0, -1, 0),
-            Triple(-1, 0, 0),
-            Triple(1, 0, 0))
-        .map { Triple(it.first + this.first, it.second + this.second, it.third + this.third) }
+            Cube(0, 0, 1),
+            Cube(0, 0, -1),
+            Cube(0, 1, 0),
+            Cube(0, -1, 0),
+            Cube(-1, 0, 0),
+            Cube(1, 0, 0))
+        .map { Cube(it.first + this.first, it.second + this.second, it.third + this.third) }
         .toList()
     }
 
@@ -32,13 +31,13 @@ fun main() {
     println(cubes.size * 6 - adj)
 
     // Part 2
-    fun outsideBounds(cube: Triple<Int, Int, Int>): Boolean {
+    fun outsideBounds(cube: Cube): Boolean {
         return cube.toList().firstOrNull { it < minDim - 1 || it > maxDim + 1 } != null
     }
 
-    val visited = mutableSetOf<Triple<Int, Int, Int>>()
-    fun bfs(startCube: Triple<Int, Int, Int>) {
-        val queue = ArrayDeque<Triple<Int, Int, Int>>()
+    val visited = mutableSetOf<Cube>()
+    fun bfs(startCube: Cube) {
+        val queue = ArrayDeque<Cube>()
         queue.add(startCube)
         while (queue.isNotEmpty()) {
             val airCube = queue.removeFirst()
@@ -58,7 +57,7 @@ fun main() {
     }
 
     // recursion stack overflows, so do BFS
-    bfs(Triple(minDim - 1, minDim - 1, minDim - 1))
+    bfs(Cube(minDim - 1, minDim - 1, minDim - 1))
 
     var result = 0
     for (cube in cubes) {
